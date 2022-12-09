4a5f6c4ec0a3fe61ba55c99431c1d61b2b2f2456
Vishal kumar <vishal.oneto11@gmail.com>
2022-12-09 10:23:29 +0530

    fixed all bugs

---
+++ hedera-reactjs-boilerplate-master/README.md
@@ -0,0 +1,73 @@
+## Introduction
+
+This is a static front end website created using the create-react-app toolset and implemented Hedera Hashgraph using hash-sdk
+
+## Built with
+
+* [Hedera Hashgraph](https://www.hedera.com/) - The enterprise-grade public network
+* [Hash-SDK](https://github.com/hashingsystems/hash.js) - The easiest way to use Hedera in JavaScript
+* [React.js](https://reactjs.org/) - a javascript library for building user interfaces
+
+## Questions & contributions
+
+If you have any questions or feedback on how we can improve this demo, [reach out](https://twitter.com/hashingsystems) to us or post a question/option on our [forum](https://dailystamp.io/).
+
+## Prerequisites
+
+This demo assumes that you have an account on the Hedera testnet. For example:
+
+```
+ACCOUNT_ID=0.0.123456789
+PRIVATE_KEY=302e020100300506032b657004220420f4361ec73dc43e568f1620a7b7ecb7330790b8a1c7620f1ce353aa1de4f0eaa6'
+```
+
+If you don’t have one, sign up using [Composer](https://chrome.google.com/webstore/detail/composer-for-hedera-hashg/hdjnnemgikeoehneddegfcmkljenlean?hl=en). You can follow [this quick tutorial](https://medium.com/hashingsystems/how-to-get-started-on-hedera-and-composer-in-5-easy-steps-2c2cbb6af09d) to get started.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `yarn test`
+
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `yarn build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `yarn eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+## Disclaimer
+
+This is just a simple demo. Please use responsibly.
+
+[Contact](https://twitter.com/hashingsystems) us if you need anything.
---
+++ hedera-reactjs-boilerplate-master/src/hedera.ts
@@ -0,0 +1,152 @@
+import * as hash from "hash-sdk";
+
+ 
+interface TopicUpdateData {
+    topicId: string;
+    memo?: string;
+    topicName?: string;
+    submitKeyList?: string|string[];
+    expirationTime?: string|number;
+  }
+  interface TopicInfoData {
+    topicId: string;
+    memo: string;
+  }
+  interface SubmitMessageData {
+    topicId: string;
+    memo: string;
+    message: string;
+  }
+ 
+ 
+  interface AccountDataLike {
+    accountId: string;
+    network: string;
+    keys?: {
+        privateKey: string;
+        publicKey?: string;
+    };
+    mnemonics?: string;
+}
+ 
+interface ContractFunctionCallData {
+    contractid: string;
+    memo: string;
+    abi: Array<any>;
+    params: Array<string|number|boolean>;
+    amount?: number;
+  }
+ 
+  interface FileRetrieveData {
+    fileId: string;
+    memo: string;
+  }
+export default {
+ 
+        /**
+    * nitializes an account for interacting with below functions
+    */
+    init : async()=> {
+        try{
+            // Setting it default to software it talks to sdk directly
+            await hash.setProvider("software");
+            const accountData: AccountDataLike= {
+                accountId: ""/*<accountId(0.0.1234)>*/,
+                network: ""/*<mainnet | testnet>*/,
+                keys:{
+                    privateKey:""/*<aplphanumeric user privatekey>*/
+                },
+                mnemonics:""/*<phrase>*/
+            };
+            await hash.setAccount(accountData);
+        }catch(e){
+            console.log("Error in intializing account:::",e);
+            throw e;
+        }
+    },
+ 
+scHelloWorld : {
+  update : async(newMessage: string) => {
+      const data: ContractFunctionCallData= {
+          memo: "smart contract call",
+          contractid: "0.0.28595",
+          abi:[{"constant":false,"inputs":[{"name":"newMessage","type":"string"}],"name":"update","payable":false,"stateMutability":"nonpayable","type":"function","contractId":{"contract":28595,"realm":0,"shard":0},"contractName":"HelloWorld"}],
+          params:[newMessage],
+          amount: 0
+      };
+      return await hash.triggerSmartContract(data);
+    },
+ 
+    message : async() => {
+      const data: ContractFunctionCallData= {
+          memo: "smart contract call",
+          contractid: "0.0.28595",
+          abi:[{"constant":true,"name":"message","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function","contractId":{"contract":28595,"realm":0,"shard":0},"contractName":"HelloWorld"}],
+          params:["afas"],
+          amount: 0
+      };
+      return await hash.triggerSmartContract(data);
+    }
+},
+ 
+ 
+ 
+filemyTestFiletxt : {
+      fileRetrieve : async() => {
+          const data: FileRetrieveData= {
+              memo: "file retrieve",
+              fileId: "0.0.28596"
+          };
+          return await hash.triggerFileRetrieve(data);
+      }
+    },
+ 
+ 
+topicjustATestTopic : {
+      topicUpdate : async(updatedData: TopicUpdateData) => {
+          const data: TopicUpdateData= {
+              topicId:"0.0.28597"
+          };
+          if(updatedData.topicName){
+              data.memo = updatedData.topicName;
+          }
+          if(updatedData.submitKeyList){
+            data.submitKeyList = updatedData.submitKeyList;
+          }
+          if(updatedData.expirationTime){
+            data.expirationTime = updatedData.expirationTime;
+          }
+          return await hash.triggerTopicUpdate(data);
+      },
+      
+ 
+      topicInfo : async() => {
+          const data: TopicInfoData= {
+              memo: "consensus topic info",
+              topicId:"0.0.28597"
+          };
+          return await hash.triggerTopicInfo(data);
+      },
+      
+ 
+      topicDelete : async() => {
+          const data: TopicInfoData= {
+              memo: "consensus topic delete",
+              topicId:"0.0.28597"
+          };
+          return await hash.triggerTopicDelete(data);
+      },
+      
+ 
+      messageSubmit : async(message: string=" ") => {
+          const data: SubmitMessageData= {
+              memo: "consensus message submit",
+              topicId:"0.0.28597",
+              message
+          };
+          return await hash.triggerMessageSubmit(data);
+      }
+    } ,
+    
+    hashSdk : hash
+};
